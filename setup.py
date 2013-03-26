964e70ca0fa9ccab9768fb13cd288c824df6b98b
Greg Novak <greg.novak@gmail.com>
2013-03-27 00:45:05 +0100

    Remove duplicate keywords.

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ setup(
     version='0.3.0dev',
     packages=['grasp',],
     license='public domain',
-    long_description=open('README.txt').read(),
     author='Greg Novak',
     author_email='greg.novak@gmail.com',
     # packages=['grasp', 'grasp.test'],
     # url='http://pypi.python.org/pypi/grasp/',
-    license='LICENSE.txt',
+    # license='LICENSE.txt',
     description='Useful introspection tools.',
     long_description=open('README.txt').read(),
 )
