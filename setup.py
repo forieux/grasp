9c086c14761e3a70693d1d842c94656b7d41cf1c
Greg Novak <greg.novak@gmail.com>
2013-03-27 13:24:00 +0100

    Change license to reference file.

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from distutils.core import setup
 setup(
     name='grasp',
     version='0.3.0dev',
-    license='public domain',
+    license='LICENSE',
     author='Greg Novak',
     author_email='greg.novak@gmail.com',
     packages=['grasp', 'grasp.test'],
     # url='http://pypi.python.org/pypi/grasp/',
-    # license='LICENSE.txt',
     description='Useful introspection tools.',
     long_description=open('README').read(),
 )
