bd67600e6a2a6819e1a4a7cb0aa5b8cc55784e8b
Greg Novak <greg.novak@gmail.com>
2013-03-27 13:15:14 +0100

    Include tests in install distribution.

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from distutils.core import setup
 setup(
     name='grasp',
     version='0.3.0dev',
-    packages=['grasp',],
     license='public domain',
     author='Greg Novak',
     author_email='greg.novak@gmail.com',
-    # packages=['grasp', 'grasp.test'],
+    packages=['grasp', 'grasp.test'],
     # url='http://pypi.python.org/pypi/grasp/',
     # license='LICENSE.txt',
     description='Useful introspection tools.',
