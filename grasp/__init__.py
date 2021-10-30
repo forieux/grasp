b3375a2d5aee89a408ba7ddce0c867bdb1bf1ae4
François Orieux <orieux@l2s.centralesupelec.fr>
2021-10-30 15:41:04 +0200

    Apply 2to3 and black to __init__.py

--- grasp/__init__.py
+++ grasp/__init__.py
@@ -1,4 +1,7 @@
-from grasp import *
+from .grasp import *
+
 # Fail gracefully if IPython not installed or if old version.
-try: from magic import load_ipython_extension, unload_ipython_extension
-except (ImportError, SyntaxError): pass
+try:
+    from .magic import load_ipython_extension, unload_ipython_extension
+except (ImportError, SyntaxError):
+    pass
